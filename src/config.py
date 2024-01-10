7f2041cbb4bc04e4dda15af802d4640b3e9df665
Arthur <arthur@corenzan.com>
2024-01-10 19:08:43 -0300

    fix: Make sure PORT is int

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 from os import environ
 
 database_url = environ.get("DATABASE_URL", "sqlite:///development.db")
-port = environ.get("PORT", 8000)
+port = int(environ.get("PORT", 8000))
 debug = bool(environ.get("DEBUG", True))
