9140bbde8bcf2bbbb9ed42a2bbcd6af111c87fa5
Arthur <arthur@corenzan.com>
2024-01-10 20:59:10 -0300

    feat: Start debug server when configured

--- src/main.py
+++ src/main.py
@@ -7,12 +7,19 @@ from .api import v1
 from .database import migrate
 
 app = FastAPI()
+app.include_router(v1.router)
 
 migrate()
 
 
-app.include_router(v1.router)
-
-
 def main():
+    if config.debug:
+        import debugpy
+
+        debugpy.listen(("localhost", 5678))
+
     uvicorn.run("src.main:app", host="0.0.0.0", port=config.port, reload=config.debug)
+
+
+if __name__ == "__main__":
+    main()
